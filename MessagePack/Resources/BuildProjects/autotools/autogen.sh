4c1828463dd21dabbf29d963c6f1cf153aaa9028
Martin Sjölund <martin.sjolund@liu.se>
2016-12-12 09:32:33 +0100

    Update autotools to call the cmake project
    msgpack-c 2.0.0 changed from autotools to cmake for their sources.

--- MessagePack/Resources/BuildProjects/autotools/autogen.sh
+++ MessagePack/Resources/BuildProjects/autotools/autogen.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-
-cd ../../C-Sources/msgpack-c/
-./bootstrap
+# Not a proper package; will be added somewhere else anyway
+mkdir -p m4 # Needed in OSX
+for f in AUTHORS ChangeLog COPYING README NEWS; do
+  test -f "$f" || touch "$f"
+done
+autoreconf --force --install
